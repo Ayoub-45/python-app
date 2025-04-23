632a5de7c0131d789de6f6bd62fa6ff6859f60f3
Ayoub-45 <ayoub.nasraoui161@yahoo.fr>
2025-04-23 15:40:37 +0100

    document code

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
 def main():
+    """
+    This is a simple console application that displays current date time
+    """
     print("Hello from a Dockerized Python app!")
     print("Current time is:", datetime.now())
 
